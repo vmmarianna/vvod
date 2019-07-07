ab5fc4d0673d6ad6abfa319cf4b788abb255a361
vmmarinna <marina9021089@gmail.com>
2019-07-07 13:33:43 +0300

    add project files

---
+++ src/peremenn.java
@@ -0,0 +1,17 @@
+public class peremenn {
+
+    public static void main(String[] args)
+    {
+        int val1;
+        int val2;
+        val1 = 1024;
+        System.out.println("Переменная val1 содержит" + " " + val1);
+
+        val2 = val1/2;
+
+        System.out.print("Переменная val2 содержит val2/2 ");
+        System.out.println(val2);
+
+
+    }
+}
